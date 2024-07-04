a45e5393e20ae4bd055901325d471faef11e3e2d
Lucas Sessi <lucas.sessi.programmer@gmail.com>
2024-07-04 14:38:54 -0400

    refactor do script da cli

--- cli.js
+++ cli.js
@@ -15,100 +15,40 @@ program
   .command('create-module <name>')
   .description('Cria um novo módulo')
   .action(async (name) => {
-
-
     const moduleDir = path.join(process.cwd(), 'src', 'modules', name);
-    const AgGrid = path.join(moduleDir, 'AgGrid');
-    const componentsDir = path.join(moduleDir, 'components');
-    const pagesDir = path.join(moduleDir, 'pages');
-    const storeDir = path.join(moduleDir, 'store');
-    const typesDir = path.join(moduleDir, 'types');
-    const validatorDir = path.join(moduleDir, 'validator');
+    const dirsToCreate = [
+      { path: moduleDir, template: `export default class ${capitalizeFirstLetter(name)} {}`, file: 'services.ts' },
+      { path: path.join(moduleDir, 'AgGrid') },
+      { path: path.join(moduleDir, 'components') },
+      { path: path.join(moduleDir, 'pages'), template: `<template>\n    <div>\n        ${name}\n    </div>\n</template>\n\n<script setup lang='ts'>\n</script>\n\n<style scoped>\n</style>\n`, file: `${name}.vue` },
+      { path: path.join(moduleDir, 'store'), template: `export class ${capitalizeFirstLetter(name)}Store {}`, file: `${name}Store.ts` },
+      { path: moduleDir, template: `export default []`, file: 'routes.ts' },
+      { path: path.join(moduleDir, 'types'), template: `export interface ${capitalizeFirstLetter(name)} {}`, file: 'index.ts' },
+      { path: moduleDir, template: `export { default as routes } from './routes'`, file: 'index.ts' },
+      { path: path.join(moduleDir, 'validator'), template: `export const ${capitalizeFirstLetter(name)} = ({}) => {};\n`, file: `${name}.ts` },
+    ];
 
+    dirsToCreate.forEach(({ path: dirPath, template, file }) => {
+      if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true });
 
-    // Criar diretórios
-    if (!fs.existsSync(moduleDir)) {
-      fs.mkdirSync(moduleDir);
-
-      // Caminho completo para o arquivo de serviços do módulo
-      const servicePath = path.join(moduleDir, `services.ts`);
-      const serviceTemplate = `export default class ${capitalizeFirstLetter(name)} {}`;
-      fs.writeFileSync(servicePath, serviceTemplate);
-
-
-      // Caminho completo para o arquivo de rotas do módulo
-      const routesPath = path.join(moduleDir, 'routes.ts');
-      const routesTemplate = `export default []`;
-      fs.writeFileSync(routesPath, routesTemplate);
-
-
-      // Caminho completo para index do módulo
-      const indexFilePath = path.join(moduleDir, 'index.ts');
-      const indexTemplate = `export { default as routes } from './routes'`;
-      fs.writeFileSync(indexFilePath, indexTemplate);
-    }
-
-    if (!fs.existsSync(AgGrid)) {
-      fs.mkdirSync(AgGrid);
-    }
-
-    if (!fs.existsSync(componentsDir)) {
-      fs.mkdirSync(componentsDir);
-    }
-
-    if (!fs.existsSync(pagesDir)) {
-      fs.mkdirSync(pagesDir);
-
-      const pagePath = path.join(pagesDir, `${name}.vue`);
-      const pageTemplate = `<template>
-    <div>
-        ${name}
-    </div>
-</template>
-
-<script setup lang='ts'>
-</script>
-
-<style scoped>
-</style>
-      `;
-
-        fs.writeFileSync(pagePath, pageTemplate);
-    }
-
-    if (!fs.existsSync(storeDir)) {
-      fs.mkdirSync(storeDir);
-
-        const storePath = path.join(storeDir, `${name}Store.ts`);
-        const storeTemplate = `export class ${capitalizeFirstLetter(name)}Store {}`;
-
-        fs.writeFileSync(storePath, storeTemplate);
-    }
-
-    if (!fs.existsSync(typesDir)) {
-      fs.mkdirSync(typesDir);
-      
-      const typesPath = path.join(typesDir, 'index.ts');
-      const typesTemplate = `export interface ${capitalizeFirstLetter(name)} {}`;
-      fs.writeFileSync(typesPath, typesTemplate);
-    }
-
-    if (!fs.existsSync(validatorDir)) {
-      fs.mkdirSync(validatorDir);
-
-      const validatorPath = path.join(validatorDir, `${name}.ts`);
-      const validatorTemplate = `export const ${capitalizeFirstLetter(name)} = ({}) => {};`;
-        fs.writeFileSync(validatorPath, validatorTemplate);
-    }
-
+        if (template && file) {
+          const filePath = path.join(dirPath, file);
+          fs.writeFileSync(filePath, template);
+        }
+      } else {
+        if (template && file) {
+          const filePath = path.join(dirPath, file);
+          fs.writeFileSync(filePath, template);
+        }
+      }
+    });
 
     console.log(`Módulo ${name} criado com sucesso!`);
   });
 
 program.parse(process.argv);
 
-
-
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
